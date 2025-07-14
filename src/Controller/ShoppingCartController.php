7a6e900ee2d20dba72731119c6356f36302d7bbd
miguelcruz <miguel.cruz@paynopain.com>
2025-07-14 20:50:28 +0200

    Create and test correctly ShoppingCartController.php it use and uuid to look for data in cache and return.

--- src/Controller/ShoppingCartController.php
+++ src/Controller/ShoppingCartController.php
@@ -2,15 +2,40 @@
 
 namespace App\Controller;
 
+use App\dto\ShoppingCartRequest;
+use App\utils\ShoppingCartUtils;
+use Exception;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Exception\ValidatorException;
+use Symfony\Contracts\Cache\CacheInterface;
 
 final class ShoppingCartController extends AbstractController
 {
-    #[Route('/shoppingCart', name: 'app_shopping_cart')]
-    public function index(): JsonResponse
+    public function __construct(
+        private CacheInterface $cache,
+        private CacheItemPoolInterface $cacheItemPool,
+    )
     {
-        return new JsonResponse('Must failed', 400);
+    }
+
+    #[Route('/shoppingCart', name: 'app_shopping_cart', methods: ['POST'])]
+    public function execute(#[MapRequestPayload] ShoppingCartRequest $request): JsonResponse
+    {
+        try {
+            $clientUUID = $request->client_uuid;
+
+            ShoppingCartUtils::getCreateCart($clientUUID, $this->cache);
+            $cart = $this->cacheItemPool->getItem($clientUUID)->get();
+
+            return new JsonResponse($cart);
+        }catch (Exception | \TypeError | ValidationFailedException | ValidatorException | HttpException $exception){
+            return new JsonResponse([$exception->getMessage()], 400);
+        }
     }
 }
