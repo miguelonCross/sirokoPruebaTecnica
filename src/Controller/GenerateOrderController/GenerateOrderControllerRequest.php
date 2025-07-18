62d15701c8d6f8c456278fff1313b4a9a2313ede
miguelcruz <miguel.cruz@paynopain.com>
2025-07-18 09:20:33 +0200

    GenerateOrderController.php

---
+++ src/Controller/GenerateOrderController/GenerateOrderControllerRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\GenerateOrderController;
+
+use Symfony\Component\Validator\Constraints as Assert;
+class GenerateOrderControllerRequest
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
+        public readonly string $client_uuid,
+    )
+    {
+    }
+}
